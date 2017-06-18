b03e56db05732af7bc5f306be37fa762c21c39d3
Sridhar Kidambi <sridharkidambi@srinivasas-mbp.attlocal.net>
2017-06-17 22:21:19 -0500

    properties

--- src/main/java/DivyaDesam/OtherTemples.java
+++ src/main/java/DivyaDesam/OtherTemples.java
@@ -1,11 +1,16 @@
 package DivyaDesam;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class OtherTemples {
 
+@Value("${OWNER_NAME}")
+public String ownerName;
+
     public  int mySum(int i,int j){
+        String m=ownerName;
         return (i+j);
     }
 }
